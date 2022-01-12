ce8563a55986600c7a05874c6315fa3dbec789d2
Mohamed-320 <muhmmadabdelaziz97@gmail.com>
2022-01-12 09:58:36 +0200

    Validate Returned Data using Assertions

--- src/test/java/reqres/in/GetSingleUserData.java
+++ src/test/java/reqres/in/GetSingleUserData.java
@@ -26,14 +26,20 @@ public class GetSingleUserData {
     @Test
     public void validateSingleUserResponseUsingAssertions() {
         Response response = get("https://reqres.in/api/users/2");
+
         System.out.println(response.getBody().asString());
         System.out.println(response.asString());
         System.out.println(response.getStatusCode());
         System.out.println(response.getHeader("content-type"));
+
 //      Status Code Validation
         int statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 200); // Assert that expected status code to be 200
+
 //        Data Returned Validation
         String responseBody = response.getBody().asString();
+        Assert.assertTrue(responseBody.contains("Janet"));
+        Assert.assertTrue(responseBody.contains("Weaver"));
+        Assert.assertTrue(responseBody.contains("janet.weaver@reqres.in"));
     }
 }
